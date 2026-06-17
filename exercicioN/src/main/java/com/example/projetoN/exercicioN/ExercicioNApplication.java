ee62fd2ee186db4d1645c8f1523bfcc3ea510fab
LeandrinPs <meegxs@gmail.com>
2026-06-17 14:48:46 -0300

    projetoNpN

---
+++ exercicioN/src/main/java/com/example/projetoN/exercicioN/ExercicioNApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetoN.exercicioN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicioNApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercicioNApplication.class, args);
+	}
+
+}
---
+++ exercicioN/src/test/java/com/example/projetoN/exercicioN/ExercicioNApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetoN.exercicioN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercicioNApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
